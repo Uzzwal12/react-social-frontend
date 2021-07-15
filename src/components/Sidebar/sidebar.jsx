6549942486e0e8cdad1413bcc94208000daa0681
Uzzwal Gupta <uzzwal.gupta@hoolime.co>
2021-07-15 22:51:33 +0530

    sidebar design done

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,7 +1,85 @@
 import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
 
 const SideBar = () => {
-  return <h1 className="sidebar">Side bar</h1>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <PlayCircleFilledOutlined className="sidebarIcon" />
+            <span className="sidebarListItemText">Video</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Group</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmark</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Event</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr"/>
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/person/3.jpeg" alt="" />
+            <span className="sidebarFriendName">jane doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/person/4.jpeg" alt="" />
+            <span className="sidebarFriendName">jane doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/person/5.jpeg" alt="" />
+            <span className="sidebarFriendName">jane doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/person/6.jpeg" alt="" />
+            <span className="sidebarFriendName">jane doe</span>
+          </li>
+          <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src="/assets/person/7.jpeg" alt="" />
+            <span className="sidebarFriendName">jane doe</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
