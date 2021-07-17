d3911e5cef97ef55485f83ee04e1ee9db125fa21
Uzzwal Gupta <uzzwal.gupta@hoolime.co>
2021-07-17 15:40:33 +0530

    right side design is done

--- src/components/RightBar/rightbar.jsx
+++ src/components/RightBar/rightbar.jsx
@@ -1,7 +1,44 @@
 import "./rightbar.css";
 
 const RightBar = () => {
-  return <h1 className="rightbar">Right bar</h1>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>pola</b> and <b>3 other friends</b> have birthday today.
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online friends</h4>
+        <ul className="rightbarFriends">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">jane doe</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/4.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Ross smith</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default RightBar;
